0078590db7b984ddcc4026d7b51f99ef3ed03c8f
cuberquest <134105195+cuberquest@users.noreply.github.com>
2024-02-29 21:07:42 -0500

    Digital clock from hours to `ms`.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-
+/**
+ * Decode time `numbers` and `strings`
+ * @param {number} time
+ * @returns {string}
+ */
+function decode(time) {
+  const s = Math.floor(time / 1000),
+  m = Math.floor(s / 60),
+  h = Math.floor(m / 60),
+  htz = h - new Date().getTimezoneOffset() / 60,
+  ms = time - s * 1000;
+  return `${
+    `${htz % 12}`.padStart(2, "0")
+  }:${
+    `${m % 60}`.padStart(2, "0")
+  }:${
+    `${s % 60}`.padStart(2, "0")
+  }.${
+    `${ms}`.padStart(3, "0")
+  }`;
+}
